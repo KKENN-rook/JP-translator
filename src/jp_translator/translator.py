c78e780d1d02b896b5d36d4c4192dc4d06ceb5e2
Kevin Ngo <kevin.kngo3@gmail.com>
2024-06-18 23:38:20 -0700

    Implemented is_japanese function

--- src/jp_translator/translator.py
+++ src/jp_translator/translator.py
@@ -1,17 +1,17 @@
 import re
 
-HIRAGANA_RE = re.compile(r'[\u3040-u309F]')
+HIRAGANA_RE = re.compile(r'[\u3040-\u309F]')
 KATAKANA_RE = re.compile(r'[\u30A0-\u30FF]')
 KANJI_RE = re.compile(r'[\u4E00-\u9FFF]')
 
 
-def is_japanese(text):
+def is_japanese(text: str) -> bool:
     """
     Checks if the input text is Japanese.
     Determined by the presence of Hiragana, Katakana, or Kanji.
 
     Args:
-        text (str): Str to check
+        text (str): str to check
 
     Returns:
         boolean: True if the text contains JP chars, else False
